8764c7a74b131184d3eaf3942081e186a0124488
ougi-dev <ougi-dev@proton.me>
2025-10-27 14:03:17 +0100

    feat: wrap MenubarDemo component in a header element for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { MenubarDemo } from "@/components/menu-bar";
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-zinc-50 dark:bg-black">
-      <MenubarDemo />
+      <header className="">
+        <MenubarDemo />
+      </header>
     </div>
   );
 }
